b41b424ba0b9e26bca32ceada87596a2603ff633
Ahmad Muhammad <ahmad@pearlcoral.com>
2018-07-17 16:41:39 +0200

    Added IO class

---
+++ IO.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SDL.h>
+
+enum color { BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX };
+
+class IO
+{
+public:
+	IO();
+	void DrawRectangle(int pX1, int pY1, int pX2, int pY2, enum color pC);
+	void ClearScreen();
+	int GetScreenHeight();
+	int InitGraph();
+	int Pollkey();
+	int Getkey();
+	int IsKeyDown(int pKey);
+	void UpdateScreen();
+};
+
